5c41db62cefc9a572ce8185eea825faea66521af
Wei Jin <410453494@qq.com>
2023-11-08 20:31:01 +0800

    minor

--- sourceCode/Go/src/github.com/openfaas/faas-netes/yaml/apply.sh
+++ sourceCode/Go/src/github.com/openfaas/faas-netes/yaml/apply.sh
@@ -1,6 +1,5 @@
 kubectl apply -f ./yaml/namespaces.yml
-PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
 kubectl -n openfaasdev create secret generic basic-auth \
 --from-literal=basic-auth-user=admin \
---from-literal=basic-auth-password="$PASSWORD"
+--from-literal=basic-auth-password=admin
 kubectl apply -f ./yaml/inuse
